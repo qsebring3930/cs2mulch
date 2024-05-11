0fa81d9233e1b0c3bc366daf886b45306282384a
Thomas <quentin.sebring@icloud.com>
2024-05-10 20:50:57 -0500

    Evaluation, Exercises, and Printables are only added to page when their respective files exist.

--- assets/js/exercises.js
+++ assets/js/exercises.js
@@ -3,166 +3,187 @@ var submit_bool = false;
 var num_score = 0;
 var avail_questions=0;
 var can_next_eval = true;
+var page = "";
+var eval_file_exist = false;
+var exer_file_exist = false;
 
-
-function populate() { // Appending the necessary elements for the exercises and evaluations portions to the page
-
-  fetch("/cs2mulch/assets/evaluations/testevaluation.txt")
+function populate() { // Appending the necessary elements for the exercises, evaluations, and printables portion to the page
+  page = window.location.href.split('/')
+  var evaluationurl = "/cs2mulch/assets/evaluations/" + page[5] + ".txt";
+  var exerciseurl = "/cs2mulch/assets/exercises/" + page[5] + ".txt";
+  if(evaluationurl){
+      var req = new XMLHttpRequest();
+      req.open('GET', evaluationurl, false);
+      req.send();
+      eval_file_exist = req.status==200;
+  }
+  if(exerciseurl){
+      var req = new XMLHttpRequest();
+      req.open('GET', exerciseurl, false);
+      req.send();
+      exer_file_exist = req.status==200;
+  }
+  fetch("/cs2mulch/assets/evaluations/" + page[5] + ".txt")
   .then((res) => res.text())
   .then((text) => {
-    var textArray = text.split('&');
-    avail_questions = parseInt(textArray[1]);
-    exercises_header = document.getElementById("exercises");
+    if (exer_file_exist) {
+      var textArray = text.split('&');
+      avail_questions = parseInt(textArray[1]);
+      exercises_header = document.getElementById("exercises");
 
-    exercise_image = document.createElement("IMG");
-    exercise_image.setAttribute("id", "exercise_image")
-    exercise_image.setAttribute("src", "/cs2mulch/assets/images/gsort10.png");
-    exercise_image.setAttribute("alt", "test image");
-    exercises_header.after(exercise_image);
+      exercise_image = document.createElement("IMG");
+      exercise_image.setAttribute("id", "exercise_image")
+      exercise_image.setAttribute("src", "/cs2mulch/assets/images/gsort10.png");
+      exercise_image.setAttribute("alt", "test image");
+      exercises_header.after(exercise_image);
 
 
-    question_bumper = document.createElement("DIV");
-    question_bumper.setAttribute("class", "bumper");
-    question_bumper.setAttribute("id","question_bumper");
-    exercise_question = document.createElement("TEXT");
-    exercise_question.setAttribute("style", "font-family:Fredoka One");
-    exercise_question.setAttribute("id","exercise_question");
-    question_bumper.appendChild(exercise_question);
-    exercise_image.after(question_bumper);
+      question_bumper = document.createElement("DIV");
+      question_bumper.setAttribute("class", "bumper");
+      question_bumper.setAttribute("id","question_bumper");
+      exercise_question = document.createElement("TEXT");
+      exercise_question.setAttribute("style", "font-family:Fredoka One");
+      exercise_question.setAttribute("id","exercise_question");
+      question_bumper.appendChild(exercise_question);
+      exercise_image.after(question_bumper);
 
-    exercise_answer_bumper = document.createElement("DIV");
-    exercise_answer_bumper.setAttribute("class", "bumper");
-    exercise_answer_bumper.setAttribute("id","exercise_answer_bumper");
-    exercise_answer = document.createElement("TEXT");
-    exercise_answer.setAttribute("class", "answer");
-    exercise_answer.setAttribute("id", "exercise_answer");
-    exercise_answer_bumper.appendChild(exercise_answer);
-    question_bumper.after(exercise_answer_bumper);
+      exercise_answer_bumper = document.createElement("DIV");
+      exercise_answer_bumper.setAttribute("class", "bumper");
+      exercise_answer_bumper.setAttribute("id","exercise_answer_bumper");
+      exercise_answer = document.createElement("TEXT");
+      exercise_answer.setAttribute("class", "answer");
+      exercise_answer.setAttribute("id", "exercise_answer");
+      exercise_answer_bumper.appendChild(exercise_answer);
+      question_bumper.after(exercise_answer_bumper);
 
-    reveal_button = document.createElement("BUTTON");
-    reveal_button.innerHTML = "Reveal Answer";
-    reveal_button.setAttribute("onclick", "revealAnswer()");
-    reveal_button.setAttribute("id", "reveal_button");
-    reveal_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black");
-    exercise_answer_bumper.after(reveal_button);
+      reveal_button = document.createElement("BUTTON");
+      reveal_button.innerHTML = "Reveal Answer";
+      reveal_button.setAttribute("onclick", "revealAnswer()");
+      reveal_button.setAttribute("id", "reveal_button");
+      reveal_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black");
+      exercise_answer_bumper.after(reveal_button);
 
-    new_exercise_button = document.createElement("BUTTON");
-    new_exercise_button.innerHTML = "Next Exercise";
-    new_exercise_button.setAttribute("onclick", "newExercise()");
-    new_exercise_button.setAttribute("id", "new_exercise_button");
-    new_exercise_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black; float: right");
-    reveal_button.after(new_exercise_button);
+      new_exercise_button = document.createElement("BUTTON");
+      new_exercise_button.innerHTML = "Next Exercise";
+      new_exercise_button.setAttribute("onclick", "newExercise()");
+      new_exercise_button.setAttribute("id", "new_exercise_button");
+      new_exercise_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black; float: right");
+      reveal_button.after(new_exercise_button);
 
-    evaluation_header = document.getElementById("evaluation");
-
-    evaluation_title_bumper = document.createElement("DIV");
-    evaluation_title_bumper.setAttribute("class", "bumper");
-    evaluation_title_bumper.setAttribute("id","evaluation_title_bumper");
-    evaluation_num = document.createElement("TEXT");
-    evaluation_num.setAttribute("id", "evaluation_num")
-    evaluation_num.setAttribute("value","1");
-    evaluation_num.setAttribute("style","font-family:Fredoka One");
-    evaluation_num.innerHTML = "Evaluation #";
-    evaluation_title_bumper.appendChild(evaluation_num);
-    score = document.createElement("TEXT");
-    score.setAttribute("id","score");
-    score.setAttribute("value","0");
-    score.setAttribute("style","float: right; font-family:Fredoka One");
-    score.innerHTML = "Score: 0/5";
-    evaluation_title_bumper.appendChild(score);
-    evaluation_header.after(evaluation_title_bumper);
-
-    evaluation_image_bumper = document.createElement("DIV");
-    evaluation_image_bumper.setAttribute("class", "bumper");
-    evaluation_image = document.createElement("IMG");
-    evaluation_image.setAttribute("id", "evaluation_image");
-    evaluation_image.setAttribute("src", "/cs2mulch/assets/images/gsort10.png");
-    evaluation_image.setAttribute("alt", "test image");
-    evaluation_image_bumper.appendChild(evaluation_image);
-    evaluation_title_bumper.after(evaluation_image_bumper);
-
-    evaluation_question_bumper = document.createElement("DIV");
-    evaluation_question_bumper.setAttribute("class", "bumper");
-    evaluation_question_bumper.setAttribute("id","evaluation_question_bumper");
-    evaluation_question = document.createElement("TEXT");
-    evaluation_question.setAttribute("id","evaluation_question");
-    evaluation_question.setAttribute("style","font-family:Fredoka One");
-    evaluation_question_bumper.appendChild(evaluation_question);
-    evaluation_image_bumper.after(evaluation_question_bumper);
-
-    checkbox_form = document.createElement("FORM");
-    for (i = 1; i < 5; i++){
-      temp_bumper = document.createElement("DIV");
-      temp_bumper.setAttribute("class", "bumper");
-      temp_choice = document.createElement("INPUT")
-      temp_choice.setAttribute("type", "checkbox");
-      temp_choice.setAttribute("style", "width:20px;height:20px; accent-color: #277214");
-      temp_choice.setAttribute("onclick","onlyOne(this.id)");
-      temp_choice.setAttribute("id", "choice_" + i);
-      temp_choice.setAttribute("value", "Choice " + i);
-      temp_choice.setAttribute("class", "choice");
-      label_temp_choice = document.createElement("LABEL");
-      label_temp_choice.setAttribute("for", "choice_" + i);
-      label_temp_choice.setAttribute("class", "choice_label");
-      label_temp_choice.innerHTML = "Choice " + i;
-      temp_bumper.appendChild(temp_choice);
-      temp_bumper.appendChild(label_temp_choice);
-      checkbox_form.appendChild(temp_bumper);
+      newExercise();
     }
-    evaluation_question_bumper.after(checkbox_form);
+    if (eval_file_exist) {
+      evaluation_header = document.getElementById("evaluation");
 
-    eval_buttons_bumper = document.createElement("DIV");
-    eval_buttons_bumper.setAttribute("class", "bumper");
-    submit_button = document.createElement("BUTTON");
-    submit_button.setAttribute("onclick","Submit()");
-    submit_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black");
-    submit_button.innerHTML = "Submit";
-    eval_buttons_bumper.appendChild(submit_button);
-    new_eval_button = document.createElement("BUTTON");
-    new_eval_button.innerHTML = "Next Evaluation";
-    new_eval_button.setAttribute("onclick", "newEvaluation()");
-    new_eval_button.setAttribute("id", "new_evaluation_button");
-    new_eval_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black; float: right");
-    eval_buttons_bumper.appendChild(new_eval_button);
-    checkbox_form.after(eval_buttons_bumper);
+      evaluation_title_bumper = document.createElement("DIV");
+      evaluation_title_bumper.setAttribute("class", "bumper");
+      evaluation_title_bumper.setAttribute("id","evaluation_title_bumper");
+      evaluation_num = document.createElement("TEXT");
+      evaluation_num.setAttribute("id", "evaluation_num")
+      evaluation_num.setAttribute("value","1");
+      evaluation_num.setAttribute("style","font-family:Fredoka One");
+      evaluation_num.innerHTML = "Evaluation #";
+      evaluation_title_bumper.appendChild(evaluation_num);
+      score = document.createElement("TEXT");
+      score.setAttribute("id","score");
+      score.setAttribute("value","0");
+      score.setAttribute("style","float: right; font-family:Fredoka One");
+      score.innerHTML = "Score: 0/5";
+      evaluation_title_bumper.appendChild(score);
+      evaluation_header.after(evaluation_title_bumper);
 
-    notif_bumper = document.createElement("DIV");
-    notif_bumper.setAttribute("class", "bumper");
-    evaluation_notif = document.createElement("TEXT");
-    evaluation_notif.setAttribute("id","notification");
-    notif_bumper.appendChild(evaluation_notif);
-    eval_buttons_bumper.after(notif_bumper);
+      evaluation_image_bumper = document.createElement("DIV");
+      evaluation_image_bumper.setAttribute("class", "bumper");
+      evaluation_image = document.createElement("IMG");
+      evaluation_image.setAttribute("id", "evaluation_image");
+      evaluation_image.setAttribute("src", "/cs2mulch/assets/images/gsort10.png");
+      evaluation_image.setAttribute("alt", "test image");
+      evaluation_image_bumper.appendChild(evaluation_image);
+      evaluation_title_bumper.after(evaluation_image_bumper);
 
-    print_bumper = document.createElement("DIV");
-    print_bumper.setAttribute("class", "bumper");
-    print_label = document.createElement("LABEL");
-    print_label.setAttribute("for","numbers");
-    print_label.innerHTML = "Print a worksheet with how many questions?";
-    print_bumper.appendChild(print_label);
-    print_dropdown = document.createElement("SELECT");
-    print_dropdown.setAttribute("name", "numbers");
-    print_dropdown.setAttribute("id","numbers");
-    for (i = 1; i < avail_questions + 1; i++) {
-      temp_option = document.createElement("OPTION");
-      temp_option.innerHTML = "" + i;
-      temp_option.setAttribute("value", "" + i);
-      print_dropdown.appendChild(temp_option);
+      evaluation_question_bumper = document.createElement("DIV");
+      evaluation_question_bumper.setAttribute("class", "bumper");
+      evaluation_question_bumper.setAttribute("id","evaluation_question_bumper");
+      evaluation_question = document.createElement("TEXT");
+      evaluation_question.setAttribute("id","evaluation_question");
+      evaluation_question.setAttribute("style","font-family:Fredoka One");
+      evaluation_question_bumper.appendChild(evaluation_question);
+      evaluation_image_bumper.after(evaluation_question_bumper);
+
+      checkbox_form = document.createElement("FORM");
+      for (i = 1; i < 5; i++){
+        temp_bumper = document.createElement("DIV");
+        temp_bumper.setAttribute("class", "bumper");
+        temp_choice = document.createElement("INPUT")
+        temp_choice.setAttribute("type", "checkbox");
+        temp_choice.setAttribute("style", "width:20px;height:20px; accent-color: #277214");
+        temp_choice.setAttribute("onclick","onlyOne(this.id)");
+        temp_choice.setAttribute("id", "choice_" + i);
+        temp_choice.setAttribute("value", "Choice " + i);
+        temp_choice.setAttribute("class", "choice");
+        label_temp_choice = document.createElement("LABEL");
+        label_temp_choice.setAttribute("for", "choice_" + i);
+        label_temp_choice.setAttribute("class", "choice_label");
+        label_temp_choice.innerHTML = "Choice " + i;
+        temp_bumper.appendChild(temp_choice);
+        temp_bumper.appendChild(label_temp_choice);
+        checkbox_form.appendChild(temp_bumper);
+      }
+
+      evaluation_question_bumper.after(checkbox_form);
+
+      eval_buttons_bumper = document.createElement("DIV");
+      eval_buttons_bumper.setAttribute("class", "bumper");
+      submit_button = document.createElement("BUTTON");
+      submit_button.setAttribute("onclick","Submit()");
+      submit_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black");
+      submit_button.innerHTML = "Submit";
+      eval_buttons_bumper.appendChild(submit_button);
+      new_eval_button = document.createElement("BUTTON");
+      new_eval_button.innerHTML = "Next Evaluation";
+      new_eval_button.setAttribute("onclick", "newEvaluation()");
+      new_eval_button.setAttribute("id", "new_evaluation_button");
+      new_eval_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black; float: right");
+      eval_buttons_bumper.appendChild(new_eval_button);
+      checkbox_form.after(eval_buttons_bumper);
+
+      notif_bumper = document.createElement("DIV");
+      notif_bumper.setAttribute("class", "bumper");
+      evaluation_notif = document.createElement("TEXT");
+      evaluation_notif.setAttribute("id","notification");
+      notif_bumper.appendChild(evaluation_notif);
+      eval_buttons_bumper.after(notif_bumper);
+
+      print_bumper = document.createElement("DIV");
+      print_bumper.setAttribute("class", "bumper");
+      print_label = document.createElement("LABEL");
+      print_label.setAttribute("for","numbers");
+      print_label.innerHTML = "Print a worksheet with how many questions?";
+      print_bumper.appendChild(print_label);
+      print_dropdown = document.createElement("SELECT");
+      print_dropdown.setAttribute("name", "numbers");
+      print_dropdown.setAttribute("id","numbers");
+      for (i = 1; i < avail_questions + 1; i++) {
+        temp_option = document.createElement("OPTION");
+        temp_option.innerHTML = "" + i;
+        temp_option.setAttribute("value", "" + i);
+        print_dropdown.appendChild(temp_option);
+      }
+      print_bumper.appendChild(print_dropdown);
+      print_button = document.createElement("BUTTON");
+      print_button.setAttribute("onclick","print_questions()");
+      print_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black; float: right");
+      print_button.innerHTML = "Print";
+      print_bumper.appendChild(print_button);
+      notif_bumper.after(print_bumper);
+
+      newEvaluation();
     }
-    print_bumper.appendChild(print_dropdown);
-    print_button = document.createElement("BUTTON");
-    print_button.setAttribute("onclick","print_questions()");
-    print_button.setAttribute("style", "border-radius:12px; font-family:Fredoka One; background-color: #277214; color: white; border-color: black; float: right");
-    print_button.innerHTML = "Print";
-    print_bumper.appendChild(print_button);
-    notif_bumper.after(print_bumper);
-
-    newExercise();
-    newEvaluation();
   })
   .catch((e) => console.error(e));
 }
 
-function onlyOne(id) {
+function onlyOne(id) { //unchecks all other checkbox inputs when user checks one
   var choices = document.getElementsByClassName("choice");
   for (var i = 0, length = choices.length; i < length; i++) {
     if (choices[i].checked) {
@@ -173,7 +194,7 @@ function onlyOne(id) {
 }
 
 function newExercise() { //pulls a new exercise from the corresponding page's exercise file and updates the corresponding elements
-  fetch("/cs2mulch/assets/exercises/testexercise.txt")
+  fetch("/cs2mulch/assets/exercises/" + page[5] + ".txt")
   .then((res) => res.text())
   .then((text) => {
     var textArray = text.split('&');
@@ -193,11 +214,11 @@ function revealAnswer() {
   document.getElementById("exercise_answer").className = "";
 }
 
-function newEvaluation(){ //pulls a new evaluation from the corresponding page's exercise file and updates the corresponding elements
+function newEvaluation(){ //pulls a new evaluation from the corresponding page's evaluation file and updates the corresponding elements
   submit_bool = false;
   var correct= ""
   if ((current_q<avail_questions || current_q == 0) && (can_next_eval == true)){
-    fetch("/cs2mulch/assets/evaluations/testevaluation.txt")
+    fetch("/cs2mulch/assets/evaluations/" + page[5] + ".txt")
     .then((res) => res.text())
     .then((text) => {
       current_q = current_q + 1;
@@ -235,7 +256,7 @@ function newEvaluation(){ //pulls a new evaluation from the corresponding page's
   }
 }
 
-function Submit() { //checks the answer given with the correct answer from the evaluation file and updates the evaluation score
+function Submit() { //compares the answer submitted with the correct answer from the evaluation file and updates the evaluation score
   var notif = document.getElementById("notification");
   if (!submit_bool){
     var correct = "";
@@ -270,7 +291,7 @@ function Submit() { //checks the answer given with the correct answer from the e
         choice_labels[user_choice].style.color = "green";
         notif.innerHTML = "Correct! Click New Evaluation and keep going!";
         num_score = num_score + 1;
-        score.innerHTML = "Score: " + num_score + "/5";
+        score.innerHTML = "Score: " + num_score + "/" + avail_questions;
         submit_bool = true;
         can_next_eval = true;
       } else {
@@ -287,7 +308,7 @@ function Submit() { //checks the answer given with the correct answer from the e
   }
 }
 
-function print_questions() { //sends a token of what page's evaluation needs to be printed and how many questions to a separate printable html file
+function print_questions() { //sends a token of what page's evaluation needs to be printed and how many questions to a separate printable md file
   num_q = document.getElementById("numbers").value;
   var name = "test"
   var myData = [ name, num_q ];
